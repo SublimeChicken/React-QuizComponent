d2037045113c505b8fb7691a6c18262f760f4b94
Adrian Arevalo <adrianarevalo13@gmail.com>
2018-11-13 17:18:41 -0600

    Completed the module

--- src/Quiz.js
+++ src/Quiz.js
@@ -14,7 +14,7 @@ class Quiz extends Component {
         }
     }
 
-    showNextQuestionHandler(){
+    showNextQuestion(){
     	this.setState({quiz_position: this.state.quiz_position+1});
     }
 
@@ -23,7 +23,7 @@ class Quiz extends Component {
     	return(
 	    	<div>
 	    		{ isQuizEnd ? <QuizEnd /> : <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position-1]}
-	    		 showNextQuestionHandler={this.showNextQuestionHandler.bind(this)} /> }
+	    		 showNextQuestionHandler={this.showNextQuestion.bind(this)} /> }
 	        </div>
 	    )
     };
